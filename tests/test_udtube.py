f8ffde31fed4cd2943db2ca31191ea65c71acbe3
DanielYakubov <Danielyak98@gmail.com>
2024-10-20 18:19:56 -0400

    adjusting tests to new CLI

--- tests/test_udtube.py
+++ tests/test_udtube.py
@@ -1,4 +1,5 @@
-from udtube_package.udtube import udtube_python_interface
+from udtube_package.UDTube.cli import udtube_python_interface
 
 if __name__ == "__main__":
     udtube_python_interface(["fit", "--config=records/configs/fit_config.yaml"])
+    udtube_python_interface(["predict", "--config=records/configs/fit_config.yaml"])
