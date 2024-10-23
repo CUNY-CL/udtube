a15ab64279bcea20b65b006b963efcec54fc712b
DanielYakubov <Danielyak98@gmail.com>
2024-10-23 01:29:04 -0400

    Test suite set up.

--- tests/test_udtube.py
+++ tests/test_udtube.py
@@ -1,3 +1,4 @@
+import difflib
 import glob
 import logging
 import os
@@ -7,11 +8,14 @@ import shutil
 from udtube_package.UDTube.cli import udtube_python_interface
 
 BASELINE_DIR = "datasets/baseline_predictions"
-BASELINE_SUFFIX = "baseline_pred.conllu"
 NUM_BASELINES = 3
 
 
 class MisconfigurationError(Exception):
+    pass
+
+
+class TestFailedError(Exception):
     pass
 
 
@@ -37,7 +41,7 @@ def pretest_validations(configs_path):
 
 def _run_model(yaml_config: str, prediction_path: str) -> None:
     try:
-        udtube_python_interface(["fit", f"--config={yaml_config}"])
+        udtube_python_interface(["fit", f"--config={yaml_config}", f"--prediction.path={prediction_path}"])
         udtube_python_interface(
             [
                 "predict",
@@ -50,15 +54,10 @@ def _run_model(yaml_config: str, prediction_path: str) -> None:
         raise e
     finally:
         # clean tmp path, but leave empty folder for convenience
-        # TODO - can probably just shutil.rmtree() tmp
-        for f in glob.iglob("records/tmp/*"):
-            if os.path.isdir(f):
-                shutil.rmtree(f)
-            else:
-                os.remove(f)
+        shutil.rmtree("records/tmp/")
 
 
-def run_models(configs_path: str, prediction_dir: str) -> None:
+def run_models(configs_path: str, prediction_dir: str, suffix: str) -> None:
     """
     Train and predict using several configs
     Args:
@@ -70,7 +69,7 @@ def run_models(configs_path: str, prediction_dir: str) -> None:
     for yaml_config in glob.iglob(f"{configs_path}/*"):
         # minor str processing to generate prediction file names
         name_prefix = re.match(r".+_", os.path.basename(yaml_config)).group(0)
-        pred_file_name = f"{prediction_dir}/{name_prefix}{BASELINE_SUFFIX}"
+        pred_file_name = f"{prediction_dir}/{name_prefix}{suffix}"
         logging.info(
             f"running {yaml_config} and recording test results to {pred_file_name}"
         )
@@ -83,9 +82,47 @@ def reset_baselines() -> None:
      Should only be done in rare circumstances when we expect a change in results going forward.
     """
     logging.warning("Baseline results reset.")
-    run_models("records/configs", BASELINE_DIR)
+    run_models("records/configs", BASELINE_DIR, "baseline_pred.conllu")
+
+
+def run_comparison(prediction_dir) -> None:
+    """
+    Comparing the newly predicted files to the baseline.
+    """
+    for current_file in glob.iglob(f"{prediction_dir}/*"):
+        name_prefix = re.match(r".{2,3}_", os.path.basename(current_file)).group(0)
+        # getting baseline
+        baselines_files = glob.glob(f"{BASELINE_DIR}/{name_prefix}*")
+        if not baselines_files or len(baselines_files) > 1:
+            raise MisconfigurationError(
+                f"Either 0 or more than one baseline file "
+                f'matches the test file prefix "{name_prefix}".'
+            )
+        baseline_file = baselines_files[0]
+        with open(current_file) as f1, open(baseline_file) as f2:
+            diff = list(difflib.unified_diff(f1.readlines(), f2.readlines(), n=1))
+        if not diff:
+            continue  # files are the same
+        else:
+            # files are not the same
+            logging.info("DIFFS:")
+            logging.info("\n".join(diff))
+            raise TestFailedError(
+                "The files predicted by the current model differ from the baseline"
+            )
 
 
 if __name__ == "__main__":
-    # reset_baselines()
-    pass
+    # reset_baselines() # break glass in case of emergency
+    pred_dir = "datasets/current_predictions"
+    try:
+        os.mkdir(pred_dir)
+    except FileExistsError:
+        pass
+    # running current version
+    run_models("records/configs", pred_dir, "current_pred.conllu")
+    run_comparison(pred_dir)
+    # clearing, this fails if TestFailedError occurs so the files can be used for debugging
+    shutil.rmtree(
+        pred_dir
+    )
